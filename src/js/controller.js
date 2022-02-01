7d835510d567cd61f298798a0dbc4dfeaf1cc4be
Ares Hovsepyan <areshovsepyan@yahoo.com>
2022-02-01 13:57:13 +0400

    1.1 implemented update servings

--- src/js/controller.js
+++ src/js/controller.js
@@ -58,8 +58,18 @@ const controlPagination = function(goToPage) {
   paginationView.render(state.search);
 };
 
+const controlServings = function(newServings) {
+  // Update the recipe serving (state)
+  model.updateServings(newServings);
+
+  // Update the recipe view
+  recipeView.render(model.state.recipe);
+
+};
+
 const init = function() {
   recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
 };

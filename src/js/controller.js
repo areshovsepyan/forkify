2171cfc6b60a444ca84d0a22aaf28094af928703
Ares Hovsepyan <areshovsepyan@yahoo.com>
2022-02-01 13:30:15 +0400

    first commit

---
+++ src/js/controller.js
@@ -0,0 +1,68 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime.js';
+
+import * as model from './model.js';
+import { getSearchResultsPage, state } from './model.js';
+
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
+import recipeView from './views/recipeView.js';
+import paginationView from './views/paginationView.js';
+
+
+const controlRecipes = async function() {
+  try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // 1. Loading the recipe
+    await model.loadRecipe(id);
+
+    // 2. Rendering the recipe
+    recipeView.render(model.state.recipe);
+
+  } catch (error) {
+    recipeView.renderError();
+  }
+};
+
+const controlSearchResults = async function() {
+  try {
+    // 0. Spinner on search
+    resultsView.renderSpinner();
+
+    // 1. Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // 2. Load search results
+    await model.leadSearchResults(query);
+
+    // 3. Render results
+    // resultsView.render(state.search.results);
+    resultsView.render(getSearchResultsPage());
+
+    // 4. Render the initial pagination buttons
+    paginationView.render(state.search);
+
+
+  } catch (error) {
+    throw error;
+  }
+};
+
+const controlPagination = function(goToPage) {
+  resultsView.render(getSearchResultsPage(goToPage));
+  paginationView.render(state.search);
+};
+
+const init = function() {
+  recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
+};
+init();
+
+
---
+++ src/js/views/paginationView.js
@@ -0,0 +1,60 @@
+import View from './View.js';
+import icons from 'url:../../img/icons.svg';
+import { NEXT_PAGE, PREV_PAGE } from '../config.js';
+
+class PaginationView extends View {
+  _parentElement = document.querySelector('.pagination');
+
+  addHandlerClick(handler) {
+    this._parentElement.addEventListener('click', function(e) {
+      const btn = e.target.closest('.btn--inline');
+      if (!btn) return;
+
+      const goToPage = +btn.dataset.goto;
+      handler(goToPage);
+    });
+  }
+
+  _generateMarkupButton(type) {
+    const currentPage = this._data.page;
+    const boolNumber = type === NEXT_PAGE ? 1 : -1;
+    const boolIcon = type === NEXT_PAGE ? 'right' : 'left';
+    return `
+          <button data-goto='${currentPage + boolNumber}' class='btn--inline pagination__btn--${type}'>
+              <svg class='search__icon'>
+                <use href='${icons}#icon-arrow-${boolIcon}'></use>
+              </svg>
+              <span>Page ${currentPage + boolNumber}</span>
+            </button>
+    `;
+  }
+
+  _generateMarkup() {
+    const currentPage = this._data.page;
+    const numPages = Math.ceil(this._data.results.length / this._data.resultsPerPage);
+    console.log(numPages);
+
+    // Page 1, and there are other pages
+    if (currentPage === 1 && numPages > 1) {
+      return this._generateMarkupButton(NEXT_PAGE);
+    }
+
+    // Last page
+    if (currentPage === numPages && numPages > 1) {
+      return this._generateMarkupButton(PREV_PAGE);
+    }
+
+    // Other page
+    if (currentPage < numPages) {
+      return `
+           ${this._generateMarkupButton(PREV_PAGE)}
+           ${this._generateMarkupButton(NEXT_PAGE)}
+      `;
+    }
+
+    // Page 1, and there are NO other pages
+    return '';
+  }
+}
+
+export default new PaginationView();

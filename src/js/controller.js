673728c20fbdd26ba1c7f3456feae07859796e2e
Ares Hovsepyan <areshovsepyan@yahoo.com>
2022-02-02 12:35:10 +0400

    implemented handlers on upload recipe, class list toggled, getting the object from list on upload click

--- src/js/controller.js
+++ src/js/controller.js
@@ -9,6 +9,7 @@ import resultsView from './views/resultsView.js';
 import recipeView from './views/recipeView.js';
 import paginationView from './views/paginationView.js';
 import bookmarksView from './views/bookmarksView.js';
+import addRecipeView from './views/addRecipeView.js';
 
 
 const controlRecipes = async function() {
@@ -86,6 +87,11 @@ const controlBookmarks = function() {
   bookmarksView.render(model.state.bookmarks);
 };
 
+const controlAddRecipe = function(newRecipe) {
+  console.log(newRecipe);
+  // Upload new recipe data
+};
+
 const init = function() {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -93,6 +99,7 @@ const init = function() {
   recipeView.addHandlerAddBookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
+  addRecipeView.addHandlerUpload(controlAddRecipe);
 };
 init();
 
---
+++ src/js/views/addRecipeView.js
@@ -0,0 +1,49 @@
+import View from './View.js';
+import icons from 'url:../../img/icons.svg';
+import { NEXT_PAGE, PREV_PAGE } from '../config.js';
+
+class AddRecipeView extends View {
+  _parentElement = document.querySelector('.upload');
+
+  _window = document.querySelector('.add-recipe-window');
+  _overlay = document.querySelector('.overlay');
+  _btnOpen = document.querySelector('.nav__btn--add-recipe');
+  _btnClose = document.querySelector('.btn--close-modal');
+
+  constructor() {
+    super();
+    this._addHandlerShowWindow();
+    this._addHandlerHideWindow();
+  }
+
+  #toggleWindow() {
+    this._overlay.classList.toggle('hidden');
+    this._window.classList.toggle('hidden');
+  }
+
+  _addHandlerShowWindow() {
+    this._btnOpen.addEventListener('click', this.#toggleWindow.bind(this));
+  }
+
+  _addHandlerHideWindow() {
+    this._btnClose.addEventListener('click', this.#toggleWindow.bind(this));
+    this._overlay.addEventListener('click', this.#toggleWindow.bind(this));
+  }
+
+  addHandlerUpload(handler) {
+    this._parentElement.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      const data = Object.fromEntries([...new FormData(this)]);
+
+      handler(data);
+    });
+  }
+
+  _generateMarkup() {
+
+  }
+
+}
+
+export default new AddRecipeView();
